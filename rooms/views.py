829c9a38c6bb25e938c0b2bca5358758eaddbd3a
Kchanghun <abcd877287@naver.com>
2021-07-23 22:58:16 +0900

    make search bar & save data which is selected into url

--- rooms/views.py
+++ rooms/views.py
@@ -1,5 +1,7 @@
 from django.utils import timezone
-from django.views.generic import ListView
+from django_countries import countries
+from django.views.generic import ListView, DetailView
+from django.shortcuts import render
 from . import models
 
 
@@ -15,3 +17,49 @@ class HomeView(ListView):
         now = timezone.now()
         context["now"] = now
         return context
+
+
+class RoomDetail(DetailView):
+    model = models.Room
+
+
+def search(request):
+    city = request.GET.get("city", "Anywhere")
+    city = str.capitalize(city)
+    country = request.GET.get("country", "KR")
+    room_type = int(request.GET.get("room_type", 0))
+    price = int(request.GET.get("price", 0))
+    guests = int(request.GET.get("guests", 0))
+    bedrooms = int(request.GET.get("bedrooms", 0))
+    beds = int(request.GET.get("beds", 0))
+    baths = int(request.GET.get("baths", 0))
+    s_amenities = request.GET.get("amenities")
+    s_facilities = request.GET.get("facilities")
+
+    form = {
+        "city": city,
+        "s_room_type": room_type,
+        "s_country": country,
+        "price": price,
+        "guests": guests,
+        "bedrooms": bedrooms,
+        "beds": beds,
+        "baths": baths,
+    }
+
+    room_types = models.RoomType.objects.all()
+    amenities = models.Amenity.objects.all()
+    facilities = models.Facility.objects.all()
+
+    choices = {
+        "countries": countries,
+        "room_types": room_types,
+        "amenities": amenities,
+        "facilities": facilities,
+    }
+
+    return render(
+        request,
+        "rooms/search.html",
+        {**form, **choices},
+    )
